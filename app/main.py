c3649ecf83fb2ccc009a964866e0283b9a1ee46a
Armando Roque <roque510@hotmail.com>
2025-06-02 00:48:54 -0600

    Update CORS middleware to allow requests from the production URL for enhanced accessibility.

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.include_router(pdf.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # or ["*"] for all origins (not recommended for production)
+    allow_origins=["http://localhost:3000","https://pdf-analyzer-sage.vercel.app"],  # or ["*"] for all origins (not recommended for production)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
